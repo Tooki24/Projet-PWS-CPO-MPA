4edd157172d3aa54b41a211c44f19545488a41b3
nodilod <nodilod@gmail.com>
2021-12-10 15:02:36 +0100

    ajout des migrations dans le .gitignor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4212ea4a1f62e0b7f23be64ecec690a7a778033d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4212ea4a1f62e0b7f23be64ecec690a7a778033d',
+            'dev_requirement' => false,
+        ),
+        'symfony/asset' => array(
+            'pretty_version' => 'v6.0.0',
+            'version' => '6.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/asset',
+            'aliases' => array(),
+            'reference' => '39ccc1f2356c34623379f1dff05e86738797a133',
+            'dev_requirement' => false,
+        ),
+    ),
+);
